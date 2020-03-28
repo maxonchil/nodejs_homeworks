6bf970c4e80569d79584ce59dd573f22a8c51394
max <maxonchil@gmail.com>
2020-03-28 12:55:56 +0200

    Fixed all bugs

--- homework3/semi-uber/Server/api/routes/login.router.js
+++ homework3/semi-uber/Server/api/routes/login.router.js
@@ -8,24 +8,28 @@ const jwt = require("jsonwebtoken");
 const log4js = require("log4js");
 const logger = log4js.getLogger();
 
+const errorHandle = (err, res) => {
+  logger.error(err);
+  res.status(400).json({ status: err });
+};
+
 router.post("/", async (req, res) => {
-  try {
-    const { password, username } = req.body;
-    const user = await User.findOne({ username });
-    const token = await jwt.sign(user.id, secret);
-    const verify = await bcrypt.compare(password, user.password);
+  const { password, username } = req.body;
 
-    if (!verify) {
-      throw new Error();
-    }
-
-    logger.info(user);
-    res.status(200).json({ id: user.id, token: token });
-  } catch (err) {
-    res.status(400).json({ status: err.name });
-    logger.error(err.name);
-    throw err;
+  const user = await User.findOne({ username });
+  if (!user) {
+    return errorHandle("Login failed", res);
   }
+  const token = await jwt.sign(user.id, secret);
+  if (!token) {
+    return errorHandle("Login failed", res);
+  }
+  const verify = await bcrypt.compare(password, user.password);
+  if (!verify) {
+    return errorHandle("Login failed", res);
+  }
+  logger.info(user);
+  res.status(200).json({ id: user.id, token: token });
 });
 
 module.exports = router;
