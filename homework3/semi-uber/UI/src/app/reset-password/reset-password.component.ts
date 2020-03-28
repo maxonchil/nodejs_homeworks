f7dd85d1ce6de139562055f7a0c11b4e5fce1135
max <maxonchil@gmail.com>
2020-03-28 20:13:07 +0200

    Maked a separate component for changing password(include delete profile button)

---
+++ homework3/semi-uber/UI/src/app/reset-password/reset-password.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl } from "@angular/forms";
+import { Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { HttpClient } from "@angular/common/http";
+import { environment as env } from "../../environments/environment";
+import { ActivatedRoute } from "@angular/router";
+
+@Component({
+  selector: "app-reset-password",
+  templateUrl: "./reset-password.component.html",
+  styleUrls: ["./reset-password.component.scss"]
+})
+export class ResetPasswordComponent implements OnInit {
+  pasRegexp: "(?=^.{6,}$)((?=.*d)(?=.*[A-Z])(?=.*[a-z])|(?=.*d)(?=.*[^A-Za-z0-9])(?=.*[a-z])|(?=.*[^A-Za-z0-9])(?=.*[A-Z])(?=.*[a-z])|(?=.*d)(?=.*[A-Z])(?=.*[^A-Za-z0-9]))^.*";
+  showChangePass: boolean = false;
+  statusOfPassChange: string;
+
+  changePasswordForm = new FormGroup({
+    newPassword: new FormControl("", [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.pattern(this.pasRegexp)
+    ])
+  });
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {}
+
+  passwordToogle() {
+    this.showChangePass = !this.showChangePass;
+  }
+  changePassword() {
+    const id = this.route.snapshot.paramMap.get("id");
+    const newPassword = this.changePasswordForm.get("newPassword").value;
+    this.http
+      .patch(`${env.baseURL}/user/${id}`, { id, newPassword })
+      .subscribe((res: any) => {
+        this.statusOfPassChange = res.message;
+        this.showChangePass = false;
+      });
+  }
+  deleteAccount() {
+    const id: string = this.route.snapshot.paramMap.get("id");
+    this.http.delete(`${env.baseURL}/user/${id}`).subscribe(() => {
+      this.router.navigate(["/"]);
+      localStorage.removeItem("JWT");
+    });
+  }
+}
--- homework3/semi-uber/UI/src/app/user-page/user-page.component.ts
+++ homework3/semi-uber/UI/src/app/user-page/user-page.component.ts
@@ -4,9 +4,7 @@ import { Component, OnInit } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { environment as env } from "../../environments/environment";
 import { ActivatedRoute } from "@angular/router";
-import { FormGroup, FormControl } from "@angular/forms";
-import { Validators } from "@angular/forms";
-import { Router } from "@angular/router";
+
 
 @Component({
   selector: "app-user-page",
@@ -14,48 +12,19 @@ import { Router } from "@angular/router";
   styleUrls: ["./user-page.component.scss"]
 })
 export class UserPageComponent implements OnInit {
-  pasRegexp: "(?=^.{6,}$)((?=.*d)(?=.*[A-Z])(?=.*[a-z])|(?=.*d)(?=.*[^A-Za-z0-9])(?=.*[a-z])|(?=.*[^A-Za-z0-9])(?=.*[A-Z])(?=.*[a-z])|(?=.*d)(?=.*[A-Z])(?=.*[^A-Za-z0-9]))^.*";
-  showChangePass: boolean = false;
-  statusOfPassChange: string;
+  
 
   user: any = {};
 
-  changePasswordForm = new FormGroup({
-    newPassword: new FormControl("", [
-      Validators.required,
-      Validators.minLength(6),
-      Validators.pattern(this.pasRegexp)
-    ])
-  });
   constructor(
     private http: HttpClient,
-    private route: ActivatedRoute,
-    private router: Router
+    private route: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
     this.getUserData();
   }
-  passwordToogle() {
-    this.showChangePass = !this.showChangePass;
-  }
-  changePassword() {
-    const id = this.route.snapshot.paramMap.get("id");
-    const newPassword = this.changePasswordForm.get("newPassword").value;
-    this.http
-      .patch(`${env.baseURL}/user/${id}`, { id, newPassword })
-      .subscribe((res: any) => {
-        this.statusOfPassChange = res.message;
-        this.showChangePass = false;
-      });
-  }
-  deleteAccount() {
-    const id: string = this.route.snapshot.paramMap.get("id");
-    this.http.delete(`${env.baseURL}/user/${id}`).subscribe(() => {
-      this.router.navigate(["/"]);
-      localStorage.removeItem("JWT");
-    });
-  }
+
 
   getUserData() {
     const token = localStorage.getItem("JWT");
